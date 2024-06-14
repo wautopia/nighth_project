795aa705dc267a38dc9c02969b0dc6766b0926eb
wautopia <gameprogrammer55@gmail.com>
2024-06-15 01:20:45 +0900

    mobileHeader見た目完成

--- src/ViewModel/header.js
+++ src/ViewModel/header.js
@@ -1,6 +1,7 @@
 import './header.css';
 import HeaderItem from '../Model/header_item';
 import HeaderPaspolLogo from '../Model/header_paspol_logo';
+import HeaderBergerMenu from '../Media/header_berger_menu.png';
 
 
 function Header () {
@@ -12,33 +13,30 @@ function Header () {
 			<div className="HeaderItemDetails">
 				<HeaderItem
 					h_link='/'
-					h_item="TOP"
-					
+					h_item="TOP"	
 				/>
-
 				<HeaderItem
 					h_link='/products'
 					h_item="PRODUCT"
-					
 				/>
-
 				<HeaderItem
 					h_link='/about'
 					h_item="ABOUT"
-					
 				/>
-
 				<HeaderItem
 					h_link='/news'
 					h_item="NEWS"
-					
 				/>
-
 				<HeaderItem
 					h_link='/contact'
 					h_item="CONTACT"
-					
 				/>
+				<div className="MobileMenu">
+					<p className="h-item-menu">
+						MENU
+						<img className="HeaderBergerMenu" src={ HeaderBergerMenu } alt=""></img>
+					</p>
+				</div>
 			</div>
 			
 		</div>
